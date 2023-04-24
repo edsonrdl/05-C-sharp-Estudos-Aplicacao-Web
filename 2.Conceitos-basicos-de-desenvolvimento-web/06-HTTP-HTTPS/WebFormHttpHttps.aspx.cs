140016d181f5c9f2ec4465675d29d6e2c8a197ae
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-04-24 00:30:52 -0300

    feat:Criado conceitos básicos

---
+++ 2.Conceitos-basicos-de-desenvolvimento-web/06-HTTP-HTTPS/WebFormHttpHttps.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2.Conceitos_basicos_de_desenvolvimento_web._06_HTTP_HTTPS
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        List<string> Contatos = new List<string>();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Contatos.Add("Luffy");
+            Contatos.Add("Zoro");
+            Contatos.Add("Sanji");
+            ddlContatos.DataSource = Contatos;
+            ddlContatos.DataBind();
+        }
+
+        protected void BtnMensagem(object sender, EventArgs e)
+        {
+           
+            
+        }
+
+        protected void ButtonMensagem_Click(object sender, EventArgs e)
+        {
+            Response.Write("Olá " + Request["TextBoxMensagem"]);
+            Response.Write("telefone " + Request["telefoneID"] + telefoneID.Value);
+
+        }
+    }
+}
---
+++ 2.Conceitos-basicos-de-desenvolvimento-web/06-HTTP-HTTPS/WebFormHttpHttps.aspx.designer.cs
@@ -0,0 +1,53 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Esse código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace _2.Conceitos_basicos_de_desenvolvimento_web._06_HTTP_HTTPS
+{
+
+
+    public partial class WebForm1
+    {
+
+        /// <summary>
+        /// Controle form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Controle TextBoxMensagem.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxMensagem;
+
+        /// <summary>
+        /// Controle telefoneID.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText telefoneID;
+
+        /// <summary>
+        /// Controle ButtonMensagem.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ButtonMensagem;
+    }
+}
